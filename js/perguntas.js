eb66d70a46ecb1cb4d10fd4d7dd5c663509995fc
mariapreuss <106261524+mariapreussgatinha@users.noreply.github.com>
2024-08-16 12:21:46 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,166 @@
+export const perguntas = [
+    {
+        enunciado: "Uma criança de 6 anos foi deixada em casa sozinha na parte da tarde, pois seus responsáveis não tinham com quem deixá-la. Qual o seu primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Não vejo problema!",
+                afirmacao: [
+                    "Você considera que, dadas as circunstâncias, a criança está segura e não está em risco imediato, e que isso pode ser uma solução temporária aceitável.",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "Você demonstra uma preocupação com a vulnerabilidade da criança e uma possível falta de supervisão adequada, levando-a a concluir que a situação precisa ser abordada com urgência para garantir a segurança da criança.",
+                ],
+                proxima: 2,
+            },
+        ]
+    },   
+    {
+        enunciado: "Você concorda que a educação com violência deve ser proibida em todas as circunstâncias, mesmo em casa?",
+        alternativas: [
+            {
+                texto: "Sim, qualquer forma de violência contra crianças é inaceitável e pode causar danos físicos e psicológicos a longo prazo.",
+                afirmacao: [
+                    "Na sua visão, é aceitável e até necessário aplicar punições físicas severas, como surras, como método de educação. Você provavelmente acredita que essa abordagem disciplinar é eficaz para corrigir comportamentos indesejados ou inadequados em crianças.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não, acho que na maioria dos casos é necessário uma surra bem dada",
+                afirmacao: [
+                    "Na sua visão, é aceitável e até necessário aplicar punições físicas severas, como surras, como método de educação. Você provavelmente acredita que essa abordagem disciplinar é eficaz para corrigir comportamentos indesejados ou inadequados em crianças.",
+                   
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você concorda que adolescentes devem ter o direito de trabalhar a partir dos 14 anos, mesmo que em trabalhos informais?",
+        alternativas: [
+            {
+                texto: "Sim, na verdade já passou da idade. Tem que trabalhar duro mesmo, para ajudar a sustentar a família.",
+                afirmacao: [
+                    "Você provavelmente considera que é importante que os adolescentes trabalhem arduamente para contribuir com a sustentação financeira da família.",
+                    
+                proxima: 3,
+            },
+            {
+                texto: "Não, acho que isso pode prejudicar o desenvolvimento educacional e psicológico.",
+                afirmacao: [
+                    "Você provavelmente acredita que os adolescentes devem priorizar sua educação e desenvolvimento pessoal antes de assumir responsabilidades de trabalho, especialmente em idade tão jovem.",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Na sua opinião, os adolescentes devem ter o direito de decidir sobre a sua orientação sexual?",
+        alternativas: [
+            {
+                texto: "Sim, acredito que é fundamental respeitar a autonomia e identidade dos adolescentes.",
+                afirmacao: [
+                    "Demonstra o seu apoio à liberdade de escolha e à autodeterminação dos adolescentes em questões relacionadas à sua sexualidade. Promovendo assim um ambiente de respeito e inclusão.",
+                   
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não, acredito que eles deveriam buscar um encaminhamento de um psicólogo/psiquiatra ou uma instituição religiosa e perceber o quanto isso é errado.",
+                afirmacao: [
+                    "Demonstra uma visão que prioriza intervenções externas para influenciar a orientação sexual dos adolescentes, possivelmente com base em crenças religiosas ou em concepções de saúde mental que não apoiam a diversidade sexual como uma norma aceitável.",
+                    
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
